0d2471ee8f53bc80d950fafa0e1b6e280ca211c7
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-08-04 22:27:30 +0200

    added split and replaceall, etc

--- strings/strings.go
+++ strings/strings.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -9,5 +10,13 @@ func main() {
 
 	var a_string = "some fucking string"
 
-	fmt.Println(strings.HasPrefix(a_string, "some"))
+	fmt.Println("has some prefix: " + strconv.FormatBool(strings.HasPrefix(a_string, "some")))
+	fmt.Println("to upper case:" + strings.ToUpper(a_string))
+	fmt.Println("it contains fucking: " + strconv.FormatBool(strings.Contains(a_string, "fucking")))
+
+	s := []string{"this is", a_string}
+
+	fmt.Println(strings.Join(s, " "))
+	fmt.Printf("%q\n", strings.Split("this is a goddamn long string", " "))
+	fmt.Println(strings.ReplaceAll(a_string, " ", "_"))
 }
